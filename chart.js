2f718b9630d2890441819537bee8ddc7d0527eb4
ranieldemstein <113078131+ranieldemstein@users.noreply.github.com>
2024-06-18 13:42:18 -0400

    Update chart.js

--- chart.js
+++ chart.js
@@ -191,4 +191,160 @@ document.addEventListener('DOMContentLoaded', function() {
         const previousData = areaSeries.data().find(data => data.time < param.time);
         const change = previousData ? calculateChange(price.value, previousData.value) : { priceChange: '0.00', percentChange: '0.00' };
         const dateStr = formatDate(param.time, currentRange);
-        toolTip.innerHTML = `<div style="color: white; font-family: 'Open Sans',
+        toolTip.innerHTML = `<div style="color: white; font-family: 'Open Sans', sans-serif;">⬤ ${symbolName}</div>
+                             <div style="font-size: 24px; margin: 4px 0px; color: white; font-family: 'Open Sans', sans-serif;">$${price.value.toFixed(2)}</div>
+                             <div style="font-size: 16px; color: ${change.priceChange >= 0 ? 'green' : 'red'}; font-family: 'Open Sans', sans-serif;">${change.priceChange >= 0 ? '+' : ''}${change.priceChange} (${change.percentChange}%)</div>
+                             <div style="font-size: 16px; color: white; font-family: 'Open Sans', sans-serif;">${dateStr}</div>`;
+      }
+    });
+
+    async function setChartRange(range) {
+      currentRange = range;
+      const stockData = await fetchStockData(range);
+      areaSeries.setData(stockData);
+      if (range === '1D') {
+        chart.applyOptions({
+          timeScale: {
+            timeVisible: true,
+            secondsVisible: false,
+            tickMarkFormatter: (time, tickMarkType, locale) => {
+              const date = new Date(time * 1000);
+              return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }); // Use local timezone
+            },
+          },
+        });
+      } else {
+        chart.applyOptions({
+          timeScale: {
+            timeVisible: true,
+            secondsVisible: false,
+            tickMarkFormatter: (time, tickMarkType, locale) => {
+              const date = new Date(time * 1000);
+              return date.toLocaleDateString('en-US'); // Use local timezone
+            },
+          },
+        });
+      }
+      const lastData = stockData[stockData.length - 1];
+      if (lastData) {
+        const previousData = stockData[stockData.length - 2];
+        const change = calculateChange(lastData.value, previousData.value);
+        setLegendText(symbolName, formatDate(lastData.time, range), formatPrice(lastData.value), change, range);
+      }
+      chart.timeScale().fitContent();
+    }
+
+    setChartRange('1D'); // Set 1D as default
+
+    // Add range buttons
+    const ranges = ['1D', '1W', '1M', '1Y'];
+    const buttonsContainer = document.getElementById('buttons-container');
+    buttonsContainer.innerHTML = ''; // Clear any existing buttons
+
+    ranges.forEach(range => {
+      const button = document.createElement('button');
+      button.innerText = range;
+      button.style = 'font-family: Arial, sans-serif; font-size: 12px; padding: 6px 12px; margin: 5px; border: none; background-color: rgba(6, 203, 248, 0.5); border-radius: 5px; cursor: pointer; color: black;';
+      button.addEventListener('click', () => setChartRange(range));
+      button.addEventListener('mouseover', () => button.style.backgroundColor = 'rgba(6, 203, 248, 0.7)');
+      button.addEventListener('mouseout', () => button.style.backgroundColor = 'rgba(6, 203, 248, 0.5)');
+      button.addEventListener('mousedown', () => button.style.backgroundColor = 'rgba(6, 203, 248, 0.9)');
+      button.addEventListener('mouseup', () => button.style.backgroundColor = 'rgba(6, 203, 248, 0.7)');
+      buttonsContainer.appendChild(button);
+    });
+
+    // Add price and percentage toggle buttons
+    const priceButton = document.getElementById('toggle-price');
+    const percentageButton = document.getElementById('toggle-percentage');
+
+    priceButton.style = 'font-family: Arial, sans-serif; font-size: 12px; padding: 6px 12px; margin: 5px; border: none; background-color: rgba(6, 203, 248, 0.5); border-radius: 5px; cursor: pointer; color: black;';
+    priceButton.addEventListener('click', () => {
+      chart.priceScale('right').applyOptions({
+        mode: LightweightCharts.PriceScaleMode.Normal,
+      });
+    });
+    priceButton.addEventListener('mouseover', () => priceButton.style.backgroundColor = 'rgba(6, 203, 248, 0.7)');
+    priceButton.addEventListener('mouseout', () => priceButton.style.backgroundColor = 'rgba(6, 203, 248, 0.5)');
+    priceButton.addEventListener('mousedown', () => priceButton.style.backgroundColor = 'rgba(6, 203, 248, 0.9)');
+    priceButton.addEventListener('mouseup', () => priceButton.style.backgroundColor = 'rgba(6, 203, 248, 0.7)');
+
+    percentageButton.style = 'font-family: Arial, sans-serif; font-size: 12px; padding: 6px 12px; margin: 5px; border: none; background-color: rgba(6, 203, 248, 0.5); border-radius: 5px; cursor: pointer; color: black;';
+    percentageButton.addEventListener('click', () => {
+      chart.priceScale('right').applyOptions({
+        mode: LightweightCharts.PriceScaleMode.Percentage,
+      });
+    });
+    percentageButton.addEventListener('mouseover', () => percentageButton.style.backgroundColor = 'rgba(6, 203, 248, 0.7)');
+    percentageButton.addEventListener('mouseout', () => percentageButton.style.backgroundColor = 'rgba(6, 203, 248, 0.5)');
+    percentageButton.addEventListener('mousedown', () => percentageButton.style.backgroundColor = 'rgba(6, 203, 248, 0.9)');
+    percentageButton.addEventListener('mouseup', () => percentageButton.style.backgroundColor = 'rgba(6, 203, 248, 0.7)');
+
+    // Add the tooltip
+    const container = document.getElementById('chart-container');
+
+    const toolTipWidth = 120; // Make tooltip narrower
+
+    // Create and style the tooltip html element
+    const toolTip = document.createElement('div');
+    toolTip.style = `width: ${toolTipWidth}px; position: absolute; display: none; padding: 8px; box-sizing: border-box; font-size: 12px; text-align: left; z-index: 1000; top: 12px; left: 12px; pointer-events: none; border-radius: 4px 4px 0px 0px; border-bottom: none; font-family: 'Open Sans', sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale;`;
+    toolTip.style.background = `transparent`; // Ensure the tooltip background is transparent
+    toolTip.style.color = 'white'; // Change tooltip text color to white
+    toolTip.style.borderColor = 'rgba( 239, 83, 80, 1)';
+    container.appendChild(toolTip);
+
+    // Create the magnifier overlay
+    const magnifierOverlay = document.createElement('div');
+    magnifierOverlay.style = `width: ${toolTipWidth}px; position: absolute; display: none; height: 100%; background: rgba(0, 0, 0, 0.1); pointer-events: none; z-index: 1001;`;
+    container.appendChild(magnifierOverlay);
+
+    // update tooltip
+    chart.subscribeCrosshairMove(param => {
+        if (
+            param.point === undefined ||
+            !param.time ||
+            param.point.x < 0 ||
+            param.point.x > container.clientWidth ||
+            param.point.y < 0 ||
+            param.point.y > container.clientHeight
+        ) {
+            toolTip.style.display = 'none';
+            magnifierOverlay.style.display = 'none';
+        } else {
+            legend.style.display = 'none';
+            const date = new Date(param.time * 1000);
+            const dateStr = currentRange === '1D' ? date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true }) : date.toLocaleDateString('en-US');
+            toolTip.style.display = 'block';
+            magnifierOverlay.style.display = 'block';
+            const data = param.seriesData.get(areaSeries);
+            const price = data.value !== undefined ? data.value : data.close;
+            const previousData = areaSeries.data().find(data => data.time < param.time);
+            const change = previousData ? calculateChange(price, previousData.value) : { priceChange: '0.00', percentChange: '0.00' };
+            toolTip.innerHTML = `<div style="color: white; font-family: 'Open Sans', sans-serif;">⬤ ${symbolName}</div>
+                                 <div style="font-size: 24px; margin: 4px 0px; color: white; font-family: 'Open Sans', sans-serif;">$${price.toFixed(2)}</div>
+                                 <div style="font-size: 16px; color: ${change.priceChange >= 0 ? 'green' : 'red'}; font-family: 'Open Sans', sans-serif;">${change.priceChange >= 0 ? '+' : ''}${change.priceChange} (${change.percentChange}%)</div>
+                                 <div style="font-size: 16px; color: white; font-family: 'Open Sans', sans-serif;">${dateStr}</div>`;
+
+            let left = param.point.x; // relative to timeScale
+            const timeScaleWidth = chart.timeScale().width();
+            const priceScaleWidth = chart.priceScale('left').width();
+            const halfTooltipWidth = toolTipWidth / 2;
+            left += priceScaleWidth - halfTooltipWidth;
+            left = Math.min(left, priceScaleWidth + timeScaleWidth - toolTipWidth);
+            left = Math.max(left, priceScaleWidth);
+
+            toolTip.style.left = left + 'px';
+            toolTip.style.top = 0 + 'px';
+
+            magnifierOverlay.style.left = `${Math.min(Math.max(param.point.x - halfTooltipWidth, priceScaleWidth), priceScaleWidth + timeScaleWidth - toolTipWidth)}px`;
+            magnifierOverlay.style.top = 0;
+            magnifierOverlay.style.width = `${toolTipWidth}px`;
+            magnifierOverlay.style.height = `${chartElement.clientHeight}px`;
+        }
+    });
+
+    chart.timeScale().fitContent();
+  }
+
+  let currentRange = '1D';
+  createStockChart();
+});
